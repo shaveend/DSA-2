9257370c258cfdc4a939a158b874ff875c8bcffe
shaveend <30971173+shaveend@users.noreply.github.com>
2018-06-26 21:45:41 +0530

    Changed the max_index to store the index of the LIS with largest potential and removed the printing of all LIS's

--- LIS.java
+++ LIS.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 import java.util.Random;
 public class LIS{
 	static void lis (int[] arr){
-		int[] seq= new int[arr.length];		//Used to find the maximum number of elements in the LIS
-		int[] order = new int[arr.length];	//Used to backtrack the LIS
-		int[] temp =new int[arr.length];	//Used to temporary store the LIS in case of multiple answers
+		int[] seq= new int[arr.length];		// used to find the  maximum number of elements in the largest increasing subsequence
+		int[] order = new int[arr.length];	// used to backtrack to find the largest increasing subsequence
+		int[] temp =new int[arr.length];	// used for printing the subsequence
 		
 		Arrays.fill(seq,1);
 		Arrays.fill(order,-1);
@@ -22,19 +22,23 @@ public class LIS{
 				if((arr[i]> arr[j]) && (seq[i]<=seq[j])){
 					seq[i]=seq[j]+1;
 					
+					if((max==seq[i]) && (arr[i]<arr[max_index])){
+						max_index=i;
+					}
+					
 					if(max<seq[i]){
 						max=seq[i];
 						max_index=i;
 					}
 					
+						
+					
 					order[i]=j;
 				}
 			}
 		}
-		System.out.println("The largest increasing Subsequence:");
-		for( int q=(arr.length-1);q>=0;q--){
-			if(seq[q]==max){
-					max_index=q;
+		System.out.println("The Largest Increasing Subsequence : ");
+		
 		
 				for(int k=max-1;k>=0;k--){
 					temp[k]=arr[max_index];
@@ -46,9 +50,7 @@ public class LIS{
 			
 				}
 			System.out.println();
-			//break;
-			}
-		}
+		
 	}
 	
 	public static void main (String args[]){
@@ -57,20 +59,23 @@ public class LIS{
 		System.out.println("Enter number of participants : ");
 		int n =reader.nextInt();
 		while(n>1000 || n<1){
-			System.out.println(" Enter New number between 1 and 1000");
+			System.out.print(" Enter New number between 1 and 1000");
 			n=reader.nextInt();
 			}
 		reader.close();
-		System.out.println(n);	
+		System.out.println();
+			
 		int arr1[]= new int[n];
 		Random rand = new Random();
 		int rand_int;
+		System.out.println("The Subsequence :");
+		
 		for (int p=0; p<n;p++){
 			rand_int=rand.nextInt(n);
 			System.out.print((arr1[p]=rand_int) +" ");
 			
 		}
-		System.out.println();
+		System.out.println("\n");
 		lis(arr1);
 	}
 }
